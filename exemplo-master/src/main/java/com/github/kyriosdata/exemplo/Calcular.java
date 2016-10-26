86b8d9194e0f6b1d3fa1c80bb6e3912d6802201d
Paulo Sales <paulojsalles@gmail.com>
2016-10-26 21:39:29 -0200

    Método funcionando / Miss tests

--- exemplo-master/src/main/java/com/github/kyriosdata/exemplo/Calcular.java
+++ exemplo-master/src/main/java/com/github/kyriosdata/exemplo/Calcular.java
@@ -5,10 +5,25 @@
  */
 package com.github.kyriosdata.exemplo;
 
+import com.github.kyriosdata.parser.Lexer;
+import com.github.kyriosdata.parser.Parser;
+
 /**
  *
- * @author aluno
+ * @author Paulo Junio Sales Rodrigues -/- 201602510
  */
 public class Calcular {
-    
+    public static void main(String[] args) {
+        int codigo = 0;
+        if(args.length > 0) {
+            Lexer tradutor = new Lexer(args[0]);
+            try {
+                Parser nome1 = new Parser(tradutor.tokenize());
+                System.out.println(nome1.expressao().valor());
+            } catch (Exception e) {
+                codigo = 1;
+            }
+        }
+        System.exit(codigo);
+    }
 }
